bdf400e584431dcd2bbd38c1ab46d3d23b8197b9
sanjuvalli2004@gmail.com <sanjuvalli2004@gmail.com>
2024-02-28 18:32:03 +0530

    back to normal

--- lib/database/models/Test.model.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const testSchema = new mongoose.Schema({
-    testdata:String
-})
-
-const TestModel = mongoose.models.Test|| mongoose.model('Test', testSchema)
-export default TestModel
