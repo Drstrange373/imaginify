e08c0e9f5d716e87a8fc72ff5e070c5fd0609306
sanjuvalli2004@gmail.com <sanjuvalli2004@gmail.com>
2024-03-03 22:40:07 +0530

    yohoho

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -1,11 +1,20 @@
 "use client"
 import { zodResolver } from "@hookform/resolvers/zod"
-import {  useForm } from "react-hook-form"
+import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { Input } from "@/components/ui/input"
-import { defaultValues } from "@/constants"
+import { aspectRatioOptions, defaultValues, transformationTypes } from "@/constants"
 import { CustomField } from "./CustomField"
 import { Form } from "@/components/ui/form"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { useState } from "react"
+import { AspectRatioKey } from "@/lib/utils"
 
 export const formSchema = z.object({
     title: z.string(),
@@ -18,7 +27,9 @@ export const formSchema = z.object({
 
 
 
-export default function TransformationForm({ action, data = null }: TransformationFormProps) {
+export default function TransformationForm({ action, data = null, userId, type, creditBalance }: TransformationFormProps) {
+    const [image, setImage] = useState(data)
+    const [newTransformation, setNewTransformation] = useState<Transformations | null>(null)
     const initialValues = data && action === 'Update' ? {
         title: data?.title,
         aspectRatio: data?.aspectRatio,
@@ -26,6 +37,7 @@ export default function TransformationForm({ action, data = null }: Transformati
         prompt: data?.prompt,
         publicId: data?.publicId,
     } : defaultValues
+    const transformationType = transformationTypes[type]
 
 
     const form = useForm<z.infer<typeof formSchema>>({
@@ -35,6 +47,10 @@ export default function TransformationForm({ action, data = null }: Transformati
 
     function onSubmit(values: z.infer<typeof formSchema>) {
         console.log(values)
+    }
+
+    function onSelectFieldHandler(value: string, onFiledChange: (value: string) => undefined) {
+
     }
 
     return (
@@ -47,6 +63,35 @@ export default function TransformationForm({ action, data = null }: Transformati
                     className="w-full"
                     render={({ field }) => <Input className="input-field" {...field} />}
                 />
+
+                {type === 'fill' &&
+                    <CustomField
+                        control={form.control}
+                        name="aspectRatio"
+                        formLabel="Aspect Ratio"
+                        className="w-full"
+                        render={({ field }) => (
+                            <Select
+                                onValueChange={(value) => onSelectFieldHandler(value, field.onChange)}
+                            >
+                                <SelectTrigger className="select-field">
+                                    <SelectValue placeholder="Select size" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    {Object.keys(aspectRatioOptions).map(option =>
+                                        <SelectItem key={option} className="select-item" value={option}>
+                                            {aspectRatioOptions[option as AspectRatioKey].label}
+                                        </SelectItem>
+                                    )}
+                                </SelectContent>
+                            </Select>
+
+                        )}
+
+
+                    />
+                }
+
             </form>
         </Form>
     )
