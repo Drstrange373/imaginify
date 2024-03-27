100894a222c3572b736bac497f0a96d115cd3e49
Sanjay <sanjuvalli2004@gmail.com>
2024-03-27 18:49:19 +0530

    added a new shadcn component and initially started to develop mediauploader component

---
+++ components/shared/MediaUploader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useToast } from '../ui/use-toast'
+
+export default function MediaUploader() {
+    const { toast } = useToast()
+    return (
+        <div>MediaUploader</div>
+    )
+}
